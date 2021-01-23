ce62a260de9d51a007c8c90b4de67cd6f07f2438
Bradley Gosiengfiao <baseballbrad12@gmail.com>
2021-01-23 01:18:14 -0800

    adding consts to const requirements, import express session, PORT creation.

---
+++ server.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const express = require('express');
+const sequelize = require('./config/connection');
+const routes = require('./controllers');
+
+// Express-Session Import
+const session = require('express-session');
+const exphandlebars = require('express-handlebars');
+
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Session Set Up
+const SetSession = {
+    secret: 'Super secret secret',
+    resave: false,
+    saveUninitialized: true,
+};
+
+app.use(session(SetSession));
+
+const handbars = exphbs.create({ helpers });
+
+app.engine('handlebars', handbars.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+app.listen(PORT, () => console.log('Now listening'));
+});
